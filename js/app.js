36212f6c5c34cfc4254ba75edb10b753a47cc990
cynthia1171 <c.isla1171@gmail.com>
2017-12-03 22:06:06 -0300

    movimiento del personaje

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
 var fondoJuego;
 var boton;
 var flappy;
+var cursores;
 
 /**se inicializa a variable juego, con la medida del juego, y donde se mostrara(div del html) */
-var juego = new Phaser.Game(144, 256, Phaser.CANVAS, 'bloque_juego');
+var juego = new Phaser.Game(370, 500, Phaser.CANVAS, 'bloque_juego');
 
 /**creamos el estado principal del juego */
 var estadoPrincipal = {
@@ -32,12 +33,16 @@ var estadoPrincipal = {
   create: function(){
 
     /**cargamos la imagen en la pantalla */
-    fondoJuego = juego.add.tileSprite(0, 0, 144, 256, 'fondo');
+    fondoJuego = juego.add.tileSprite(0, 0, 370, 500, 'fondo');
 
     flappy = juego.add.sprite(100, 100, 'pajaros');
 
     flappy.frame = 1;
     flappy.animations.add('vuelo', [0,1,2], 10, true);
+
+    /**crea todas las teclas del cursor: arriba, abajo ... */
+    cursores = juego.input.keyboard.createCursorKeys();
+    
     /**agregamos la imagen del personaje al juego(x,y,nombreSrite) 
     flappy = juego.add.sprite(juego.width/2, juego.height/2, 'pajaro');
     flappy.anchor.setTo(0.5);
@@ -49,6 +54,7 @@ var estadoPrincipal = {
      * rotar sprite, valor positivo lo rota segun las manecillas del reloj
      * flappy.angle = 90;
      * 
+     * teclaDerecha = juego.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
      * 
      */
     
@@ -77,6 +83,24 @@ var estadoPrincipal = {
     */
     flappy.animations.play('vuelo');
 
+    /**
+     * isDown: acion cuando aprieto la tecla, isUp: detiene la accion 
+     * if(teclaDerecha.isDown){
+      flappy.position.x += 1;
+    }*/
+    //movimiento del personaje
+    if(cursores.right.isDown){
+      flappy.position.x +=1;
+    }
+    if (cursores.left.isDown) {
+      flappy.position.x -= 1;
+    }
+    if (cursores.up.isDown) {
+      flappy.position.y -= 1;
+    }
+    if (cursores.down.isDown) {
+      flappy.position.y += 1;
+    }
 
   }
 
